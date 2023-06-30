e13409ed9987ca3921593d506bb58f6fec46fb25
Kibet Mutai <109361613+Kibet-mutai@users.noreply.github.com>
2023-06-30 16:42:13 +0300

    tipster/user home

---
+++ components/navs/TipsterLeft.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="w-full flex flex-col mt-8 px-4">
+        <div class="h-72">
+            <NuxtLink to="/tipster" class="font-bold text-xl cursor-pointer">Tipstimate</NuxtLink>
+        </div>
+        <div class="flex flex-col space-y-6">
+            <div class="flex flex-row space-x-3 items-center">
+                <div class="w-10 h-10 rounded-full">
+                    <nuxt-img
+                        src="/images/rick-gebhardt-F3v6OP0cl8g-unsplash.jpg"
+                        alt="user's profile"
+                        class="w-full h-full rounded-full object-cover"
+                    ></nuxt-img>
+                </div>
+                <NuxtLink t0="" class="font-bold cursor-pointer">Profilo</NuxtLink>
+            </div>
+            <div class="flex flex-row space-x-3 items-center py-6">
+                <Icon name="mdi:plus" class="h-6 w-6 cursor-pointer"></Icon>
+                <NuxtLink to="" class="font-bold cursor-pointer">Create Evento</NuxtLink>
+            </div>
+            <div class="flex flex-row space-x-3 items-center">
+                <Icon name="mdi:home" class="h-6 w-6 cursor-pointer"></Icon>
+                <NuxtLink to="" class="font-bold cursor-pointer">Home</NuxtLink>
+            </div>
+            <div class="flex flex-row space-x-3 items-center">
+                <Icon name="mdi:trophy" class="h-6 w-6 cursor-pointer"></Icon>
+                <NuxtLink to="" class="font-bold cursor-pointer">Contest</NuxtLink>
+            </div>
+            <div class="flex flex-row space-x-3 items-center">
+                <Icon name="mdi:medal-outline" class="h-6 w-6 cursor-pointer"></Icon>
+                <NuxtLink to="" class="font-bold cursor-pointer">classifica Tipster</NuxtLink>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ pages/tipster/index.vue
@@ -0,0 +1,190 @@
+<template>
+    <nuxt-layout name="home">
+        <div class="flex flex-col items-center mt-8 w-full">
+            <div class="max-w-[500px] w-full">
+                <div class="flex space-x-4 relative">
+                    <NuxtLink to="/home" class="items-center flex flex-col">
+                        <div class="border-pink-500 rounded-full border-2 w-[66px] h-[66px]">
+                            <nuxt-img
+                                src="/images/luke-porter-PtfrMfx-X7o-unsplash.jpg"
+                                alt="Profile images"
+                                class="rounded-full object-center object-cover h-full w-full"
+                            >
+                            </nuxt-img>
+                        </div>
+                        <div class="font-bold pt-2">
+                            <h1>User 1</h1>
+                        </div>
+                    </NuxtLink>
+                    <NuxtLink to="/home" class="items-center flex flex-col">
+                        <div class="border-pink-500 rounded-full border-2 w-[66px] h-[66px]">
+                            <nuxt-img
+                                src="/images/luke-porter-PtfrMfx-X7o-unsplash.jpg"
+                                alt="Profile images"
+                                class="rounded-full object-center object-cover h-full w-full"
+                            >
+                            </nuxt-img>
+                        </div>
+                        <div class="font-bold pt-2">
+                            <h1>User 1</h1>
+                        </div>
+                    </NuxtLink>
+                    <NuxtLink to="/home" class="items-center flex flex-col">
+                        <div class="border-pink-500 rounded-full border-2 w-[66px] h-[66px]">
+                            <nuxt-img
+                                src="/images/luke-porter-PtfrMfx-X7o-unsplash.jpg"
+                                alt="Profile images"
+                                class="rounded-full object-center object-cover h-full w-full"
+                            >
+                            </nuxt-img>
+                        </div>
+                        <div class="font-bold pt-2">
+                            <h1>User 1</h1>
+                        </div>
+                    </NuxtLink>
+                    <NuxtLink to="/home" class="items-center flex flex-col">
+                        <div class="border-pink-500 rounded-full border-2 w-[66px] h-[66px]">
+                            <nuxt-img
+                                src="/images/luke-porter-PtfrMfx-X7o-unsplash.jpg"
+                                alt="Profile images"
+                                class="rounded-full object-center object-cover h-full w-full"
+                            >
+                            </nuxt-img>
+                        </div>
+                        <div class="font-bold pt-2">
+                            <h1>User 1</h1>
+                        </div>
+                    </NuxtLink>
+                    <NuxtLink to="/home" class="items-center flex flex-col">
+                        <div class="border-pink-500 rounded-full border-2 w-[66px] h-[66px]">
+                            <nuxt-img
+                                src="/images/luke-porter-PtfrMfx-X7o-unsplash.jpg"
+                                alt="Profile images"
+                                class="rounded-full object-center object-cover h-full w-full"
+                            >
+                            </nuxt-img>
+                        </div>
+                        <div class="font-bold pt-2">
+                            <h1>User 1</h1>
+                        </div>
+                    </NuxtLink>
+                    <NuxtLink to="/home" class="items-center flex flex-col">
+                        <div class="border-pink-500 rounded-full border-2 w-[66px] h-[66px]">
+                            <nuxt-img
+                                src="/images/luke-porter-PtfrMfx-X7o-unsplash.jpg"
+                                alt="Profile images"
+                                class="rounded-full object-center object-cover h-full w-full"
+                            >
+                            </nuxt-img>
+                        </div>
+                        <div class="font-bold pt-2">
+                            <h1>User 1</h1>
+                        </div>
+                    </NuxtLink>
+                </div>
+                <div class="flex-flex-col space-y-6 py-8">
+                    <div class="border shadow-md flex flex-col p-4 rounded-md shadow-pink-500">
+                        <div class="flex justify-between">
+                            <div class="flex space-x-4 items-center">
+                                <div class="border-pink-500 rounded-full border-2 w-[40px] h-[40px]">
+                                    <nuxt-img
+                                        src="/images/luke-porter-PtfrMfx-X7o-unsplash.jpg"
+                                        alt="Profile images"
+                                        class="rounded-full w-full h-full object-center object-cover"
+                                    >
+                                    </nuxt-img>
+                                </div>
+                                <h3 class="font-bold">User 1</h3>
+                                <div
+                                    class="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 items-center px-2 rounded-md"
+                                >
+                                    <h6 class="text-white text-sm">sponsorizzato</h6>
+                                </div>
+                            </div>
+                            <div class="w-32">
+                                <button
+                                    type="button"
+                                    class="text-xs text-white font-medium w-full items-center border rounded-3xl py-2 bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 hover:transition-all delay-300"
+                                >
+                                    Follow Username
+                                </button>
+                            </div>
+                        </div>
+                        <div class="bg-gray-300 p-3 mt-2 rounded-md divide-y divide-solid divide-neutral-400">
+                            <div class="py-4 px-2">
+                                <div class="items-start flex flex-col">
+                                    <h1 class="font-bold">Milan - Hellas Verona</h1>
+                                    <div class="flex space-x-2 items-center">
+                                        <Icon name="mdi:soccer"></Icon>
+                                        <h6 class="text-gray-500 text-sm font-thin">Football - Italy - Serie A</h6>
+                                    </div>
+                                    <div class="pl-2 flex">
+                                        <span class="h-6 w-4 border-l border-b border-gray-400 relative">
+                                            <div class="absolute left-4 px-2 top-3 flex justify-between space-x-48">
+                                                <h1 class="font-medium whitespace-nowrap">U/O Tempo IT: 0.5 UNDER</h1>
+                                                <h1 class="font-bold">2.85</h1>
+                                            </div>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="py-4 px-2">
+                                <div class="items-start flex flex-col">
+                                    <h1 class="font-bold">Milan - Hellas Verona</h1>
+                                    <div class="flex space-x-2 items-center">
+                                        <Icon name="mdi:soccer"></Icon>
+                                        <h6 class="text-gray-500 text-sm font-thin">Football - Italy - Serie A</h6>
+                                    </div>
+                                    <div class="pl-2 flex">
+                                        <span class="h-6 w-4 border-l border-b border-gray-400 relative">
+                                            <div class="absolute left-4 px-2 top-3 flex justify-between space-x-48">
+                                                <h1 class="font-medium whitespace-nowrap">U/O Tempo IT: 0.5 UNDER</h1>
+                                                <h1 class="font-bold">2.85</h1>
+                                            </div>
+                                        </span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="flex justify-between pt-3">
+                            <div class="flex space-x-3">
+                                <div class="text-center items-center flex flex-col space-y-2">
+                                    <Icon name="mdi:heart" class="text-red-600 h-6 w-6"></Icon>
+                                    <h2 class="font-medium">10.5k</h2>
+                                </div>
+                                <div class="text-center items-center flex flex-col space-y-2">
+                                    <Icon name="mdi:share-variant-outline" class="h-6 w-6"></Icon>
+                                    <h2 class="font-medium">Condividi</h2>
+                                </div>
+                                <div class="text-center items-center flex flex-col space-y-2 px-4">
+                                    <Icon name="mdi:alarm-plus" class="h-6 w-6"></Icon>
+                                    <h2 class="font-medium">Agguinga Storia</h2>
+                                </div>
+                            </div>
+                            <div
+                                class="bg-green-500 flex justify-around p-2 items-center rounded-lg h-14 divide-x divide-solid w-full mt-4"
+                            >
+                                <div class="flex">
+                                    <nuxt-img
+                                        src="/images/th-2035313927 (1).png"
+                                        alt="bookmaker"
+                                        class="w-16 object-cover"
+                                    ></nuxt-img>
+                                </div>
+                                <div class="flex flex-col px-2">
+                                    <h4 class="text-white text-xs">Giocata</h4>
+                                    <h4 class="font-bold text-white text-xs">258</h4>
+                                    <h4 class="text-white text-xs">Volte</h4>
+                                </div>
+                                <div class="flex flex-col px-2">
+                                    <h4 class="text-white text-xs">Probabilita</h4>
+                                    <h4 class="text-white font-bold text-xs">1.17</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </nuxt-layout>
+</template>
