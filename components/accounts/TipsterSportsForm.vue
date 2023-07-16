02fd2576ca7aefa7c805c86cc93d99c41268b27f
Amos Kipyegon <dittoamos@gmail.com>
2023-07-16 23:10:38 +0300

    chore: Update tipster signup form

--- components/accounts/TipsterSportsForm.vue
+++ components/accounts/TipsterSportsForm.vue
@@ -1,9 +1,16 @@
 <script setup lang="ts">
-const sports = ['Football', 'Basketball', 'Hockey', 'Tennis', 'Cricket']
-const countries = ['Italy', 'England', 'Spain', 'United States', 'France', 'Germany']
-const selectedSport = ref('')
-const selectedOtherSport = ref('')
-const selectedCountry = ref('')
+const sports = ['Football', 'Basketball', 'Hockey', 'Tennis', 'Cricket'];
+const countries = [
+    'Italy',
+    'England',
+    'Spain',
+    'United States',
+    'France',
+    'Germany',
+];
+const selectedSport = ref('');
+const selectedOtherSport = ref('');
+const selectedCountry = ref('');
 </script>
 <template>
     <div class="w-full flex flex-col">
@@ -13,30 +20,36 @@ const selectedCountry = ref('')
                     type="text"
                     id="penname"
                     name="penname"
-                    class="peer text-sm bg-transparent h-9 w-full rounded-sm text-gray-700 placeholder-transparent ring-1 px-2 ring-c-seperator focus:ring-neutral-400 focus:outline-none"
+                    class="peer text-sm bg-c-light h-10 w-full rounded-lg text-gray-700 placeholder-transparent ring-1 px-2 ring-c-seperator/60 focus:ring-neutral-400/60 focus:outline-none"
                     placeholder="Pen name"
                 /><label
                     for="penname"
-                    class="absolute cursor-text left-0 -top-2 text-xs text-gray-500 bg-c-light mx-1 px-1 peer-placeholder-shown:text-sm peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2 peer-focus:-top-2 peer-focus:text-t-gray peer-focus:text-xs transition-all"
+                    class="absolute cursor-text left-1.5 -top-2 text-xs text-gray-500 bg-c-light mx-1 px-1 peer-placeholder-shown:text-sm peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2 peer-focus:-top-2 peer-focus:text-t-gray peer-focus:text-xs transition-all"
                     >Pen name</label
                 >
             </div>
             <div class="w-full">
                 <select v-model="selectedCountry" class="select-input">
                     <option value="" disabled selected>Nationality</option>
-                    <option v-for="country in countries" :value="country">{{ country }}</option>
+                    <option v-for="country in countries" :value="country">
+                        {{ country }}
+                    </option>
                 </select>
             </div>
             <div class="w-full">
                 <select v-model="selectedSport" class="select-input">
                     <option value="" disabled selected>Favorite Sport</option>
-                    <option v-for="sport in sports" :value="sport">{{ sport }}</option>
+                    <option v-for="sport in sports" :value="sport">
+                        {{ sport }}
+                    </option>
                 </select>
             </div>
             <div class="w-full">
                 <select v-model="selectedOtherSport" class="select-input">
                     <option value="" disabled selected>Other sports</option>
-                    <option v-for="sport in sports" :value="sport">{{ sport }}</option>
+                    <option v-for="sport in sports" :value="sport">
+                        {{ sport }}
+                    </option>
                 </select>
             </div>
         </div>
--- components/ui/SelectDateForm.vue
+++ components/ui/SelectDateForm.vue
@@ -14,10 +14,12 @@ const months = [
     'November',
     'December',
 ];
+
+const currentYear = new Date().getFullYear();
 const years = Array.from(
     { length: 100 },
-    (_, index) => new Date().getFullYear() - index
-);
+    (_, index) => currentYear - index
+).filter((year) => currentYear - year > 18);
 
 const props = defineProps<{
     selectedDay: string;
@@ -31,54 +33,71 @@ const emits = defineEmits<{
 }>();
 </script>
 <template>
-    <div class="w-full flex flex-col gap-y-1 mb-1">
-        <div class="dob-input w-full flex flex-row justify-between items-center">
-            <select
-                :value="selectedDay"
-                @input="
-                    emits(
-                        'update:selectedDay',
-                        ($event.target as HTMLSelectElement).value
-                    )
-                "
-                class="select-field"
-            >
-                <option value="" disabled selected>Day</option>
-                <option v-for="day in days" :value="day">{{ day }}</option>
-            </select>
-            <select
-                :value="selectedMonth"
-                @input="
-                    emits(
-                        'update:selectedMonth',
-                        ($event.target as HTMLSelectElement).value
-                    )
-                "
-                class="select-field"
-            >
-                <option value="" disabled selected>Month</option>
-                <option v-for="(month, index) in months" :value="index + 1">
-                    {{ month }}
-                </option>
-            </select>
-            <select
-                :value="selectedYear"
-                @input="
-                    emits(
-                        'update:selectedYear',
-                        ($event.target as HTMLSelectElement).value
-                    )
-                "
-                class="select-field"
-            >
-                <option value="" disabled selected>Year</option>
-                <option v-for="year in years" :value="year">{{ year }}</option>
-            </select>
-        </div>
+    <div class="w-full grid-flow-col grid items-center gap-x-2 gap-y-2">
+        <select
+            :value="selectedDay"
+            @input="
+                emits(
+                    'update:selectedDay',
+                    ($event.target as HTMLSelectElement).value
+                )
+            "
+            class="select-field custom-select"
+        >
+            <option value="" disabled selected>Day</option>
+            <option v-for="day in days" :value="day">{{ day }}</option>
+        </select>
+        <select
+            :value="selectedMonth"
+            @input="
+                emits(
+                    'update:selectedMonth',
+                    ($event.target as HTMLSelectElement).value
+                )
+            "
+            class="select-field custom-select"
+        >
+            <option value="" disabled selected>Month</option>
+            <option v-for="(month, index) in months" :value="index + 1">
+                {{ month }}
+            </option>
+        </select>
+        <select
+            :value="selectedYear"
+            @input="
+                emits(
+                    'update:selectedYear',
+                    ($event.target as HTMLSelectElement).value
+                )
+            "
+            class="select-field custom-select"
+        >
+            <option value="" disabled selected>Year</option>
+            <option v-for="year in years" :value="year">{{ year }}</option>
+        </select>
     </div>
 </template>
 <style scoped>
 .select-field {
     @apply text-sm bg-inherit border border-c-seperator rounded-lg text-gray-600 py-2 px-3 focus:ring-blue-500 focus:border-blue-500 outline-none transition duration-200;
+}
+
+.custom-select {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    appearance: none;
+    background-image: none;
+    padding-right: 6px;
+    background-image: url('/icons/MdiChevronDown.svg');
+    background-position: right center;
+    background-origin: content-box;
+    background-repeat: no-repeat;
+    background-size: 18px 18px;
+    filter: grayscale(100%);
+}
+
+/* Style the dropdown arrow to make it look like a custom icon */
+.custom-select::-ms-expand {
+    display: none;
 }
 </style>
