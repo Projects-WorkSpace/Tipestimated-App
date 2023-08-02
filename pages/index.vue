a4bd85012c354480c030fa4b95f32fd6a0e84d97
Kibet Mutai <109361613+Kibet-mutai@users.noreply.github.com>
2023-08-02 18:25:51 +0300

    Add auth middleware

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-// definePageMeta({
-// 	middleware: "auth",
-// });
+definePageMeta({
+    middleware: 'auth',
+});
 </script>
 <template>
-	<div class="w-full">
-		<NuxtLayout name="users">
-			<div class="w-full flex flex-col gap-y-4 py-4 mt-2">
-				<UiSliderHorizontal />
-				<div class="w-full flex flex-col gap-y-4 mt-2">
-					<SectionsPostCard />
-					<SectionsPostCard />
-					<SectionsPostCard />
-				</div>
-			</div>
-		</NuxtLayout>
-	</div>
+    <div class="w-full">
+        <NuxtLayout name="users">
+            <div class="w-full flex flex-col gap-y-4 py-4 mt-2">
+                <UiSliderHorizontal />
+                <div class="w-full flex flex-col gap-y-4 mt-2">
+                    <SectionsPostCard />
+                    <SectionsPostCard />
+                    <SectionsPostCard />
+                </div>
+            </div>
+        </NuxtLayout>
+    </div>
 </template>
